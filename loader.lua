45459eaa46d55af7410c859bf88a6e12fdc1ce26
pinkgithub1 <94822403+pinkgithub1@users.noreply.github.com>
2022-08-04 22:39:23 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 6073366803 or 6242296404 then
+if game.PlaceId == 6073366803 or game.PlaceId == 6242296404 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/pinkgithub1/happyrun-ragerun/main/happyrunragerun.lua"))()
 else
     print('wrong game doofus')
